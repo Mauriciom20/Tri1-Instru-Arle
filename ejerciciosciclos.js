9b6667d4f93dba2c9e6852fbaffd8cc2e5ec58a5
Mauriciom20 <168799203+Mauriciom20@users.noreply.github.com>
2024-05-20 01:02:05 -0500

    Update ejerciciosciclos.js

--- ejerciciosciclos.js
+++ ejerciciosciclos.js
@@ -96,6 +96,127 @@ do{
 }while(f >= 5 && f <=10)*/
 
 //5. Repita un menú de 3 opciones hasta que el usuario digite el numero 4
+
+/*let numero = parseInt(prompt("ingrese una opcion \n1. opcion 1\n2. opcion 2\n3. opcion 3\n4. opcion 4, terminar progrma"));
+while(numero!=4)
+    {
+        switch (numero) {
+            case 1:
+                alert("opcion 1");
+                break;
+            case 2:
+                alert("opcion 2");
+                break;
+            case 3:
+                alert("opcion 3");
+                break;
+        
+            default:
+                alert("opcion invalida");
+                break;
+        }
+        numero = parseInt(prompt("ingrese una opcion \n1. opcion\n2. opcion\n3. opcion\n4. terminar progrma"));
+    }
+    alert('saliste del programa');*/
+
+/*for(let i = parseInt(prompt("ingrese una opcion \n1. opcion 1\n2. opcion 2\n3. opcion 3\n4. opcion 4, terminar progrma")); i!=4; i)
+    switch (i!=4) {
+    case 1:
+        alert("opcion 1");
+        break;
+    case 2:
+        alert("opcion 2");
+        break;
+    case 3:
+        alert("opcion 3");
+        break;
+
+    default:
+        alert("opcion invalida");
+        i = parseInt(prompt("ingrese una opcion \n1. opcion 1\n2. opcion 2\n3. opcion 3\n4. opcion 4, terminar progrma"))
+        break;
+    }
+
+alert('saliste del programa');*/
+
+/*let i
+do{ switch (i!=4) {
+    case 1:
+        alert("opcion 1");
+        break;
+    case 2:
+        alert("opcion 2");
+        break;
+    case 3:
+        alert("opcion 3");
+        break;
+
+    default:
+        i = parseInt(prompt("ingrese una opcion \n1. opcion 1\n2. opcion 2\n3. opcion 3\n4. opcion 4, terminar progrma"))
+        break;
+    }
+}while(i!=4) {alert('saliste del programa')}*/
+    
 //6. Usando un cilo imprima los números 3,6,9,12
+
+/*let i = 3
+
+while (i <= 12){
+    console.log(i)
+    i+=3
+}*/
+
+/*for(let i = 3; i<= 12; console.log(i), i+=3){}*/
+
+/*let f = 3
+do{
+    console.log(f)
+    f+=3
+}while(f <= 12)*/
+
 //7. Imprima la palabra “hola” 5 veces
+
+/*let i = 0;
+while (i < 5) {
+    console.log("hola");
+    i++;
+}*/
+
+/*for (let i = 0; i < 5; i++) {
+    console.log("hola");
+}*/
+
+/*let i = 0;
+do {
+    console.log("hola");
+    i++;
+} while (i < 5);*/
+
 //8. Imprima los números del 1 al 7 sin imprimir el 3
+
+/*let i = 1
+while (i <= 7) {
+    if (i === 3) {
+        i++
+    };
+    console.log(i);
+    i++;
+    
+}*/
+
+/*for (let i = 1; i <= 7; i++) {
+    if (i === 3) {
+        i++
+    }
+    console.log(i)
+}*/
+
+/*let i = 1;
+do {
+    if (i === 3) {
+        i++;
+        continue; // Salta la iteración si i es igual a 3
+    }
+    console.log(i);
+    i++;
+} while (i <= 7);*/
